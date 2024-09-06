6924a7d5a94e78e01b0061c192644e80ff4bdb1a
Jana Veliskova <veliskova@gmail.com>
2024-09-05 21:45:53 -0400

    update the dialog

--- javascript.js
+++ javascript.js
@@ -11,22 +11,30 @@
 
 
                 const forms = document.getElementsByClassName('j-email-form');
+                // I don't love this
+                const okButton = document.getElementById("okButton");
+                const closeButton = document.getElementById("closeButton");
 
                 for (let i = 0; i < forms.length; i++) {
                     const form = forms[i];
                     // I have no idea why the bind works and why it needs a null, but it works
                     form?.addEventListener('submit' , _getEmail.bind(null, form));
                 }
+                // I really don't like this, it feels clunky
+                okButton.addEventListener('click' , _toggleDialog);
+                closeButton.addEventListener('click' , _toggleDialog);
+
                 // No need for this to bubble up, nothing else needs to use this click
                 event.stopPropagation();
             });
         }
 
         // dialog opening and closing logic
-        const _toggleDialog = (paragraph) => {
+        const _toggleDialog = () => {
             const dialog = document.getElementById('emailDialog');
 
             if(dialog.open) {
+                const paragraph = document.getElementById('dialog-message');
                 paragraph.innerHTML = "";
                 dialog.close();
             } else {
@@ -51,7 +59,7 @@
                     paragraph = document.getElementById('dialog-message');
             
             paragraph.append(message1);
-            _toggleDialog(paragraph);
+            _toggleDialog();
         }
 
         const _init = () => {
